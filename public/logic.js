31ffb0c4eef8637dd963cce482b92515fbc09e8b
Dennis Mullen <dkmullen@gmail.com>
2017-12-05 09:36:36 -0500

    Add jq plugin and code to logic file to begin process of enabling token retrevial

--- public/logic.js
+++ public/logic.js
@@ -11,3 +11,35 @@ let addUser = () => {
     console.log('User created');
   });
 };
+
+let addAnother = () => {
+  var clientId = "MyApp";
+  var clientSecret = "MySecret";
+
+  var authorizationBasic = $.base64.btoa(clientId + ':' + clientSecret);
+
+  $.ajax({
+      type: 'POST',
+      url: '/users',
+      data: { email: 'nick@dm.com', password: '12345', grant_type: 'password' },
+      dataType: "json",
+      contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+      xhrFields: {
+         withCredentials: true
+      },
+      // crossDomain: true,
+      headers: {
+         'Authorization': 'Basic ' + authorizationBasic
+      },
+      //beforeSend: function (xhr) {
+      //},
+      success: function (result) {
+         var token = result;
+      },
+      //complete: function (jqXHR, textStatus) {
+      //},
+      error: function (req, status, error) {
+      console.log(error);
+      }
+  });
+};
