9921c3c0139a33da91a44ca5b373dc7f1b66f085
Dennis Mullen <dkmullen@gmail.com>
2018-06-10 19:21:52 -0400

    Work on auth

--- server.js
+++ server.js
@@ -3,33 +3,25 @@
 const express = require('express'),
   mongoose = require('mongoose'),
   bodyParser = require('body-parser'),
+  logger = require('morgan'),
+  routes = require('./routing/routes'),
+  user = require('./models/user'),
   app = express();
 
-const User = require('./user'),
+const User = require('./models/user'),
   port = 3000;
 
-  mongoose.connect('mongodb://localhost:27017/playground-app', { useMongoClient: true });
-  mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/playground-app');
+mongoose.Promise = global.Promise;
+app.set('secret', 'fake-secret');
 
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
-  app.use(express.static(__dirname + '/public'));
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.use('/routes', routes);
 
-  // create a new user
-  app.post('/users', (req, res) => {
-      let user = new User({
-        email: req.body.email,
-        password: req.body.password
-      });
-
-      user.save().then(() => {
-        console.log(user);
-        res.send(user);
-      })
-      .catch((e) => {
-        res.status(400).send(e);
-      });
-  });
+routes(app);
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
